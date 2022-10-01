9baf4bc20993f5e1d3de8917b16dc09e3c600a69
Taylor Knopp <taylor@knoppfmaily.net>
2022-09-30 18:19:08 -0600

    Initial Project Setup and Yaml Library Install

---
+++ Cisco CML IOS Configuration Extractor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+namespace Cisco_CML_IOS_Configuration_Extractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
